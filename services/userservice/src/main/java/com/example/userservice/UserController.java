e34912ed463568e3a2e18dfcea8a2fca72e3a752
Surya Dasari <suryadasari0@gmail.com>
2026-02-05 12:53:39 +0530

    v1.0 – Stable authentication platform with CI/CD and OpenShift deploymentDevelop (#1)
    * Add CI Jenkinsfile for develop branch
    
    * update of frontend stage in Jenkinsfile
    
    * Add Docker build and push to CI pipeline
    
    * Add Docker build and push to CI pipeline
    
    * Update Jenkinsfile with openshift deployment
    
    * adding Deployment files
    
    * Fix OpenShift YAML syntax, use latest image and always pull
    
    * Fix apiservice env vars and image pull policy
    
    * Enable actuator health and prometheus metrics
    
    * Stabilize Postgres, secrets, and OpenShift deployments
    
    * Latest Jenkins File
    
    * login page setup
    
    * Fix login validation using PasswordEncoder
    
    * Fix password encoding and login validation
    
    * nothing
    
    * full jenkins file
    
    * fixed  bugs
    
    * Fix userservice datasource to use env vars
    
    * Stabilize CI/CD, Postgres StatefulSet, secrets, and service configs
    
    * Configure Nexus distributionManagement for Java services
    
    * Update Jenkinsfile with latest on feb4
    
    * Update Jenkinsfile with pwd chenges
    
    * Fix userservice actuator health and datasource config
    
    * Update Jenkinsfile with forcefull rollout
    
    * Update Jenkinsfile updtaed with working
    
    * APP is working fine after fixing config and secrets

--- services/userservice/src/main/java/com/example/userservice/UserController.java
+++ services/userservice/src/main/java/com/example/userservice/UserController.java
@@ -1,28 +1,43 @@
 package com.example.userservice;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
 public class UserController {
 
     private final UserRepository repo;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public UserController(UserRepository repo) {
         this.repo = repo;
     }
 
+    // Signup
     @PostMapping("/users")
     public String create(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         repo.save(user);
         return "User created";
     }
 
+    // Login validation
     @PostMapping("/users/validate")
     public boolean validate(@RequestBody User user) {
-        return repo.findByUsernameAndPassword(
-                user.getUsername(),
-                user.getPassword()
-        ).isPresent();
+        Optional<User> dbUser = repo.findByUsername(user.getUsername());
+
+        if (dbUser.isEmpty()) {
+            return false;
+        }
+
+        return passwordEncoder.matches(
+                user.getPassword(),
+                dbUser.get().getPassword()
+        );
     }
 }
+
